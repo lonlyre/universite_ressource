0328ba95e7a10d1742902c51017db4404ce9475d
lonlyre <limulelocari@gmail.com>
2026-02-06 11:48:49 +0100

    debut du projet sans vrai fonctionnement logique encore

---
+++ biblioteque.py
@@ -0,0 +1,25 @@
+class ressource:
+    
+    __init__(self, id:int, name:str, type : str , status : str ):
+        self.id = id
+        self.name = name
+        self.type = type
+        self.status = status
+        
+
+class universite :
+    
+        __init__(self):
+            self.bibliotheque 
+            self.equipement
+            self.salle
+        
+        
+        
+        
+        
+class user:
+    
+    __init__(self):
+        self.userid 
+        
